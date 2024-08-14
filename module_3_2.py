e3b054d36ea61bfebba4ad494bb16ebdff870a7c
AlexCo0711 <alexco@list.ru>
2024-08-14 12:33:43 +0300

    Add files via upload
    Домашняя работа по уроку "Способы вызова функции"

--- module_3_2.py
+++ module_3_2.py
@@ -1,10 +1,10 @@
 # Домашняя работа по уроку "Способы вызова функции"
 
 # Создание функции проверки рассылки писем
-def send_email(message, recipient, *, sen='university.help@gmail.com'):
+def send_email(message, recipient, *, sender='university.help@gmail.com'):
 # создание списка проверки правильности email
-    mail = [rec, sen]
-# цикл проверки списка mail установленным правилам
+    mail = [recipient, sender]
+# цикл проверки списка mail установленным правилам (@, .com, .ru, .net)
     for i in mail:
         if i.find('@') != -1 and (i.find('.com', len(i) - 4) != -1 or i.find('.ru', len(i) - 3) != -1 or i.find('.net',len(i) - 4) != -1):
             adr = True  # email согласно правил
@@ -13,16 +13,18 @@ def send_email(message, recipient, *, sen='university.help@gmail.com'):
             adr = False # email не по правилам
             break
     if adr == False: # условие неправильного email
-        print('Невозможно отправить письмо с адреса', sen, 'на адрес', rec, f'.')
-    elif 'university.help@gmail.com' == sen != rec: # условие правильного email
-        print('Письмо успешно отправлено с адреса', sen, 'на адрес', rec, f'.')
-    elif sen == rec: # условие равенства email jnghfdbntkz b gjkexfntkz
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient, f'.')
+    elif 'university.help@gmail.com' == sender != recipient: # условие правильного email
+        print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient, f'.')
+    elif sender == recipient: # условие равенства email jnghfdbntkz b gjkexfntkz
         print('Нельзя отправить письмо самому себе!')
     else: # в случае когда email отправителя не соответствует шаблону
-        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sen, 'на адрес', rec, f'.')
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient, f'.')
     return
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sen='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sen='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sen='urban.teacher@mail.ru')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender = 'urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender = 'urban.teacher@mail.ru')
+# дополнительная проверка отправка самому себе с адреса согласно шаблона отправителя
+send_email('Напоминаю самому себе', 'university.help@gmail.com')
