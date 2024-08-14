91a4b324bf114be5c8a689d0705aa89775d8bf68
AlexCo0711 <alexco@list.ru>
2024-08-14 12:37:00 +0300

    Add files via upload
    Домашняя работа по уроку "Способы вызова функции"

--- module_3_2.py
+++ module_3_2.py
@@ -2,29 +2,31 @@
 
 # Создание функции проверки рассылки писем
 def send_email(message, recipient, *, sender='university.help@gmail.com'):
-# создание списка проверки правильности email
+    # создание списка проверки правильности email
     mail = [recipient, sender]
-# цикл проверки списка mail установленным правилам (@, .com, .ru, .net)
+    # цикл проверки списка mail установленным правилам (@, .com, .ru, .net)
     for i in mail:
-        if i.find('@') != -1 and (i.find('.com', len(i) - 4) != -1 or i.find('.ru', len(i) - 3) != -1 or i.find('.net',len(i) - 4) != -1):
+        if i.find('@') != -1 and (i.find('.com', len(i) - 4) != -1 or i.find('.ru', len(i) - 3) != -1 or i.find('.net',
+                                                                                                                len(i) - 4) != -1):
             adr = True  # email согласно правил
             continue
         else:
-            adr = False # email не по правилам
+            adr = False  # email не по правилам
             break
-    if adr == False: # условие неправильного email
+    if adr == False:  # условие неправильного email
         print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient, f'.')
-    elif 'university.help@gmail.com' == sender != recipient: # условие правильного email
+    elif 'university.help@gmail.com' == sender != recipient:  # условие правильного email
         print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient, f'.')
-    elif sender == recipient: # условие равенства email jnghfdbntkz b gjkexfntkz
+    elif sender == recipient:  # условие равенства email jnghfdbntkz b gjkexfntkz
         print('Нельзя отправить письмо самому себе!')
-    else: # в случае когда email отправителя не соответствует шаблону
+    else:  # в случае когда email отправителя не соответствует шаблону
         print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient, f'.')
     return
 
+
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender = 'urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender = 'urban.teacher@mail.ru')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
 # дополнительная проверка отправка самому себе с адреса согласно шаблона отправителя
 send_email('Напоминаю самому себе', 'university.help@gmail.com')
